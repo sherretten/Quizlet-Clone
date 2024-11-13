6434ef0cbd3c0bcd633ae4aa6278ceabfdd36470
Nate <nathaniels2016@gmail.com>
2024-11-12 18:25:43 -0800

    setup db

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
-import Link from "next/link";
+import { db } from '../server/db';
 
-export default function HomePage() {
+export default async function HomePage() {
+
+	const tickets = await db.query.posts.findMany();
+
 	return (
 		<main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
 			First changes, whoop
+			{tickets.map((t, i) => {
+				<div key={i}>{t}</div>
+			})}
 		</main>
 	);
 }
